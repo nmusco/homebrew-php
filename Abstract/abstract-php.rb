f8e9f10edf4385913a97feec2e273f33c31a3a2c
naatye <nmusco@users.noreply.github.com>
2017-06-25 20:41:17 -0400

    High Sierra Update

--- Abstract/abstract-php.rb
+++ Abstract/abstract-php.rb
@@ -68,8 +68,8 @@ class AbstractPhp < Formula
     deprecated_option "with-apache" => "with-httpd24"
     deprecated_option "with-apache22" => "with-httpd22"
 
-    depends_on "homebrew/apache/httpd24" => :optional
-    depends_on "homebrew/apache/httpd22" => :optional
+    depends_on "nmusco/apache/httpd24" => :optional
+    depends_on "nmusco/apache/httpd22" => :optional
 
     option "with-cgi", "Enable building of the CGI executable (implies --without-fpm)"
     option "with-debug", "Compile with debugging symbols"
